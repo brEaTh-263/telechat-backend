11854297387f502c7f881b7fa362716dacc2dc59
Keshav Chhaparia <chhapariakeshav@gmail.com>
2021-06-17 18:13:39 +0530

    send message details

--- index.js
+++ index.js
@@ -10,6 +10,7 @@ const { Server } = require("socket.io");
 const io = new Server(server);
 const { mongoDbURI } = require("./config");
 const { Room } = require("./models/room");
+const { User } = require("./models/user");
 app.use(bodyParser.json());
 app.use("/auth", authRouter);
 app.use("/user", userRouter);
@@ -30,8 +31,12 @@ io.use(async (socket, next) => {
 	const receiverId = socket.handshake.auth.receiverId;
 	const roomId = socket.handshake.auth.roomId;
 	// console.log("Room Id" + roomId);
+	console.log("Middleware was called!!!");
 	if (!_id) {
 		return next(new Error("invalid _id"));
+	}
+	if (!receiverId) {
+		return next();
 	}
 	if (roomId) {
 		const room = await Room.findById(roomId);
@@ -67,13 +72,72 @@ io.use(async (socket, next) => {
 });
 
 io.on("connection", (socket) => {
-	console.log("Users connected");
+	console.log("User connected" + socket.id);
 	socket.on("disconnect", () => {
-		socket.leave(socket.roomId.toString());
+		if (socket.roomId) socket.leave(socket.roomId.toString());
 		console.log("User disconnected");
 	});
-	socket.emit("room", { roomId: socket.roomId });
-	socket.join(socket.roomId.toString());
+
+	socket.on("get_my_chats", async ({ _id }) => {
+		const allRooms = await Room.find({
+			userIds: _id,
+		});
+		// console.log(allRooms);
+		const populatedRooms = await Promise.all(
+			allRooms.map(async (room) => {
+				let x = [];
+
+				let newuser = await User.findById(room.userIds[0]);
+
+				x.push(newuser);
+				newuser = await User.findById(room.userIds[1]);
+				x.push(newuser);
+
+				return {
+					_id: room._id,
+					messages: room.messages,
+					userIds: x,
+				};
+			})
+		);
+
+		io.to(socket.id).emit("your_rooms", populatedRooms);
+	});
+	if (socket.roomId) socket.emit("room", { roomId: socket.roomId });
+	if (socket.roomId) {
+		console.log("Trying to join a roooooommmm!!!");
+		socket.join(socket.roomId.toString());
+	}
+	socket.on("connect_me_to_room", async ({ _id, receiverId }) => {
+		console.log("connect_me_to_room CALLED");
+		console.log("Trying to join a roooooommmm!!!");
+		const userIds = [receiverId, _id];
+
+		const availableRoom = await Room.findOne({
+			userIds: {
+				$size: userIds.length,
+				$all: [...userIds],
+			},
+		});
+		let existingRoom;
+		if (availableRoom) {
+			socket._id = _id;
+			socket.roomId = availableRoom._id;
+			existingRoom = availableRoom;
+		} else {
+			const room = new Room({
+				userIds: userIds,
+				messages: [],
+			});
+			await room.save();
+			socket.roomId = room._id;
+			existingRoom = room;
+		}
+		console.log(socket.roomId);
+
+		socket.join(socket.roomId.toString());
+		socket.emit("room", { roomId: socket.roomId, existingRoom });
+	});
 	socket.on("private message", async ({ content, to }) => {
 		console.log("Room Id" + socket.roomId);
 		const room = await Room.findById(socket.roomId);
